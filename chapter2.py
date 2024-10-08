84a7dd9ec1cfadc1ce15658e11835705f559a3b4
fs203 <fs2031@icloud.com>
2024-10-08 23:51:41 +0900

    firstCommit

---
+++ chapter2.py
@@ -0,0 +1,61 @@
+# print("Yankees rule!")
+# print("But not in Boston")
+# print("Yankees rule,", "but not in Boston!")
+
+# pi = 3
+# radius = 11
+# area = pi * (radius**2)
+# radius = 14
+# print(radius)
+
+# x, y = 2, 3
+# x, y = y, x
+
+# print("x =", x)
+# print("y =", y)
+
+# x = 9
+# if x % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+# print("Done with conditional")
+
+# x = 10
+
+# if x % 2 == 0:
+#     if x % 3 == 0:
+#         print("Divisible by 2 and 3")
+#     else:
+#         print("Divisible by 2 and not by 3")
+# elif x % 3 == 0:
+#     print("Divisible by 3 and not by 2")
+
+# x = 10
+# y = 4
+# z = 99
+
+# if x < y and x < z:
+#     print("x is least")
+# elif y < z:
+#     print("y is least")
+# else:
+#     print("z is least")
+
+# length
+# print(len("abc"))
+
+# slice
+# text = "Hello World"
+# sliced_text = text[0:5]
+# print(sliced_text)  # "Hello"
+
+name = input("Enter your name: ")
+print("Are you really" + name + "?")
+
+answer = input("yes or no:")
+
+if answer.lower() == "yes":
+    print("you can use this room.")
+elif answer.lower() == "no":
+    print("plase type your correct name.")
---
+++ chapter3.py
@@ -0,0 +1,9 @@
+x = int(input("Enter an unteger: "))
+# ans = 0
+# while ans**3 < abs(x):
+# if ans**3 != abs(x):
+#     print(x, 'is not a perfect cube')
+# else:
+#     if x < 0:
+#         ans = -ans
+#     print('Cube root of', x, 'is', ans)
