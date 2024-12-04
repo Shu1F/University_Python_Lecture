07ea23a3b52238db6f1a3fc7c97025ac2eb48f82
fs203 <fs2031@icloud.com>
2024-12-04 10:06:22 +0900

    update

---
+++ chapter10/lecture.py
@@ -0,0 +1,16 @@
+class intDict(object):
+
+    def __init__(self, numBuckets):
+
+        self.buckets = []
+        self.numBuckets = numBuckets
+        for i in range(numBuckets):
+            self.buckets.append([])
+
+    def addEntry(self, key, dictVal):
+        hashBucket = self.buckets[key % self.numBuckets]
+        for i in range(len(hashBucket)):
+            if hashBucket[i][0] == key:
+                hashBucket[i] = (key, dictVal)
+                return
+        hashBucket.append((key, dictVal))
