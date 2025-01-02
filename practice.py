6bd5421f98b1f5cb19d04551c8a7bd347d4ce206
fs203 <fs2031@icloud.com>
2025-01-02 14:15:20 +0900

    update

--- practice.py
+++ practice.py
@@ -7,18 +7,34 @@
 
 # print(toPrint)
 
-number = input("Please type 10 integer").split()
+# number = input("Please type 10 integer").split()
 
-number = [int(num) for num in number]
+# number = [int(num) for num in number]
 
-odd_Numbers = []
+# odd_Numbers = []
 
-for num in number:
-    if num % 2 == 1:
-        odd_Numbers.append(num)
+# for num in number:
+#     if num % 2 == 1:
+#         odd_Numbers.append(num)
 
-if odd_Numbers:
-    max_number = max(odd_Numbers)
-    print(f"The largest odd number is: {max_number}")
-else:
-    print("There are no odd numbers in the input.")
+# if odd_Numbers:
+#     max_number = max(odd_Numbers)
+#     print(f"The largest odd number is: {max_number}")
+# else:
+#     print("There are no odd numbers in the input.")
+
+
+# x = int(input("Enter an integer: "))
+# ans = 0
+
+
+# while ans**3 < abs(x):
+#     ans = ans + 1
+# if ans**3 != abs(x):
+#     print(x, "is not a perfect cube")
+# else:
+#     if x < 0:
+#         ans = -ans
+#     print("Cube root of", x, "is", ans)
+
+num = int(input("please type integer"))
