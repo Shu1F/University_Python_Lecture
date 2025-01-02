5c93110a826ca706ff09e4354eb5f91cf2547b89
fs203 <fs2031@icloud.com>
2025-01-02 13:33:50 +0900

    update

--- practice.py
+++ practice.py
@@ -1,8 +1,24 @@
-numXs = int(input("How many times should I print the letter X?"))
-toPrint = ""
+# numXs = int(input("How many times should I print the letter X?"))
+# toPrint = ""
 
-while numXs != 0:
-    toPrint = toPrint + "X"
-    numXs = numXs - 1
+# while numXs != 0:
+#     toPrint = toPrint + "X"
+#     numXs = numXs - 1
 
-print(toPrint)
+# print(toPrint)
+
+number = input("Please type 10 integer").split()
+
+number = [int(num) for num in number]
+
+odd_Numbers = []
+
+for num in number:
+    if num % 2 == 1:
+        odd_Numbers.append(num)
+
+if odd_Numbers:
+    max_number = max(odd_Numbers)
+    print(f"The largest odd number is: {max_number}")
+else:
+    print("There are no odd numbers in the input.")
