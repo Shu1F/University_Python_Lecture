1d4bcfb92cfcefba14db1978813c67ceef4dedd7
fs203 <fs2031@icloud.com>
2025-01-03 19:08:05 +0900

    update

--- practice.py
+++ practice.py
@@ -134,16 +134,16 @@ import matplotlib.pyplot as plt
 # printName(lastName="秀一", firstName="藤池", reverse=False)
 
 
-def f(x):
-    y = 1
-    x = x + y
-    print("x =", x)
-    return x
+# def f(x):
+#     y = 1
+#     x = x + y
+#     print("x =", x)
+#     return x
 
 
-x = 3
-y = 2
-z = f(x)
-print("z =", z)
-print("x =", x)
-print("y =", y)
+# x = 3
+# y = 2
+# z = f(x)
+# print("z =", z)
+# print("x =", x)
+# print("y =", y)
